885f17dbe144aaf2b3d4fc239feff6af3932b679
Manoj <manojpeta30@gmail.com>
2023-03-17 14:39:22 +0530

    first commit

---
+++ src/products.js
@@ -0,0 +1,31 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 70000.00,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 120000.00,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Camera",
+    price: 40000,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "IPhone-SE",
+    price: 50000.00,
+    productImage: product1,
+  },
+];
